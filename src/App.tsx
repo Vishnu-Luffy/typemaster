a6eaf41856a5630b9a243deec007e5e763857b84
Vishnu-Luffy <tenalivishnuvardhanreddy007@gmail.com>
2025-01-06 08:28:27 +0530

    typemaster

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import {Button} from "./components/ui/button"
+import {CardWithForm} from "./components/Tcard"
+function App() {
+  
+
+  return (
+    <>
+    <div className="flex justify-center items-center">
+    <Button>
+         hello
+         </Button>
+         <CardWithForm/>
+
+        
+
+    </div>
+      
+    </>
+  )
+}
+
+export default App
